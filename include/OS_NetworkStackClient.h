a41f62c2033fb6011ae1ac69a63e2798c5a45f8a
Thomas Böhm <thomas.boehm@hensoldt-cyber.de>
2021-06-24 11:21:30 +0200

    [REF] Remove hardcoded function defines / add missing headers

--- include/OS_NetworkStackClient.h
+++ include/OS_NetworkStackClient.h
@@ -5,6 +5,8 @@
 #pragma once
 
 #include "OS_Dataport.h"
+#include "OS_Error.h"
+#include "OS_Network.h"
 
 typedef struct
 {
@@ -16,60 +18,3 @@ OS_Error_t
 OS_NetworkStackClient_init(
     OS_NetworkStackClient_SocketDataports_t* config);
 
-//------------------------------------------------------------------------------
-// RPC API, prefix "networkStack_rpc" comes from CAmkES RPC, the rest from the
-// interface method list.
-//------------------------------------------------------------------------------
-OS_Error_t
-networkStack_rpc_socket_create(
-    int                        domain,
-    int                        type,
-    OS_NetworkSocket_Handle_t* pHandle);
-
-OS_Error_t
-networkStack_rpc_socket_accept(
-    OS_NetworkSocket_Handle_t  handle,
-    OS_NetworkSocket_Handle_t* pHandleClient,
-    uint16_t                   port);
-
-OS_Error_t
-networkStack_rpc_socket_bind(
-    OS_NetworkSocket_Handle_t handle,
-    uint16_t                  port);
-
-OS_Error_t
-networkStack_rpc_socket_listen(
-    OS_NetworkSocket_Handle_t handle,
-    int                       backlog);
-
-OS_Error_t
-networkStack_rpc_socket_connect(
-    OS_NetworkSocket_Handle_t handle,
-    const char*               name,
-    uint16_t                  port);
-
-OS_Error_t
-networkStack_rpc_socket_close(
-    OS_NetworkSocket_Handle_t handle);
-
-OS_Error_t
-networkStack_rpc_socket_write(
-    OS_NetworkSocket_Handle_t handle,
-    size_t* pLen);
-
-OS_Error_t
-networkStack_rpc_socket_read(
-    OS_NetworkSocket_Handle_t handle,
-    size_t* pLen);
-
-OS_Error_t
-networkStack_rpc_socket_recvfrom(
-    OS_NetworkSocket_Handle_t handle,
-    size_t*                   plen,
-    OS_Network_Socket_t*      src_socket);
-
-OS_Error_t
-networkStack_rpc_socket_sendto(
-    OS_NetworkSocket_Handle_t handle,
-    size_t*                   pLen,
-    OS_Network_Socket_t       dst_socket);
